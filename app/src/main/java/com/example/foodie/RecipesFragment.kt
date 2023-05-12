9acc15868679ae55512dcc3d0f0d922df44aeb6f
Darshan Satra <darshansatra1@gmail.com>
2023-05-12 17:24:24 -0400

    add shimmer layout

--- app/src/main/java/com/example/foodie/RecipesFragment.kt
+++ app/src/main/java/com/example/foodie/RecipesFragment.kt
@@ -5,14 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.foodie.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
+
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get()=  _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+    ): View {
+        _binding = FragmentRecipesBinding.inflate(inflater,container,false)
+
+        binding.shimmerFrameLayout.showShimmer(true)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
