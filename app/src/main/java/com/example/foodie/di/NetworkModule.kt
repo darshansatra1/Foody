831ae00cb50feb38058fd68bcc987f75340a9ed3
Darshan Satra <darshansatra1@gmail.com>
2023-05-22 16:15:58 -0400

    create repository

--- app/src/main/java/com/example/foodie/di/NetworkModule.kt
+++ app/src/main/java/com/example/foodie/di/NetworkModule.kt
@@ -1,8 +1,7 @@
 package com.example.foodie.di
 
-import com.example.foodie.Constants.Companion.BASE_URL
-import com.example.foodie.FoodRecipesApi
-import com.google.gson.Gson
+import com.example.foodie.util.Constants.Companion.BASE_URL
+import com.example.foodie.data.network.FoodRecipesApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -47,7 +46,7 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideApiService(retrofit: Retrofit):FoodRecipesApi{
+    fun provideApiService(retrofit: Retrofit): FoodRecipesApi {
         return retrofit.create(FoodRecipesApi::class.java)
     }
 }
