a952d2ba4d1a0b8bffa8e647e780e114ccf8e1cf
Darshan Satra <darshansatra1@gmail.com>
2023-05-22 16:00:36 -0400

    add Network Module

--- app/src/main/java/com/example/foodie/Constants.kt
+++ app/src/main/java/com/example/foodie/Constants.kt
@@ -1,7 +1,6 @@
 package com.example.foodie
 
 class Constants {
-
     companion object{
         const val API_KEY = "28bb28b70ce54cde80da5c20259f580c"
         const val BASE_URL = "https://api.spoonacular.com"
