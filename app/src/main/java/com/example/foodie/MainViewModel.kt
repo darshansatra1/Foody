216170e9ef59093db81246fd7c0f1bccfbd614a1
Darshan Satra <darshansatra1@gmail.com>
2023-05-22 22:30:09 -0400

    show data response in recycler view

--- app/src/main/java/com/example/foodie/MainViewModel.kt
+++ app/src/main/java/com/example/foodie/MainViewModel.kt
@@ -4,18 +4,20 @@ import android.app.Application
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.foodie.data.Repository
 import com.example.foodie.models.FoodRecipe
 import com.example.foodie.util.NetworkResult
+import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.launch
 import retrofit2.Response
 import javax.inject.Inject
 
-@ViewModelScoped
+@HiltViewModel
 class MainViewModel @Inject constructor(
     private val repository: Repository,
     application: Application,
@@ -34,6 +36,7 @@ class MainViewModel @Inject constructor(
                 val response = repository.remote.getRecipes(queries)
                 recipesResponse.value = handleFoodRecipesResponse(response)
             }catch (e:java.lang.Exception){
+                Log.e("ERROR",e.toString())
                 recipesResponse.value = NetworkResult.Error("Recipes not found")
             }
         }else{
@@ -41,7 +44,7 @@ class MainViewModel @Inject constructor(
         }
     }
 
-    private fun handleFoodRecipesResponse(response: Response<FoodRecipe>): NetworkResult<FoodRecipe>? {
+    private fun handleFoodRecipesResponse(response: Response<FoodRecipe>): NetworkResult<FoodRecipe> {
         when{
             response.message().toString().contains("timeout")->{
                 return NetworkResult.Error("Timeout")
@@ -49,9 +52,10 @@ class MainViewModel @Inject constructor(
             response.code()==402->{
                 return NetworkResult.Error("API Key Limited")
             }
-            response.body()!!.results.isNotEmpty()->{
+            response.body()!!.results.isEmpty()->{
                 return NetworkResult.Error("Recipes not found")
             }
+
             response.isSuccessful->{
                 val foodRecipes = response.body()
                 return NetworkResult.Success(foodRecipes!!)
--- app/src/main/java/com/example/foodie/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foodie/ui/fragments/recipes/RecipesFragment.kt
@@ -1,25 +1,95 @@
 package com.example.foodie.ui.fragments.recipes
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.foodie.MainViewModel
+import com.example.foodie.adapters.RecipesAdapter
 import com.example.foodie.databinding.FragmentRecipesBinding
+import com.example.foodie.util.Constants.Companion.API_KEY
+import com.example.foodie.util.NetworkResult
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class RecipesFragment : Fragment() {
 
     private var _binding: FragmentRecipesBinding? = null
-    private val binding get()=  _binding!!
+    private val binding get() = _binding!!
+    private val adapter by lazy { RecipesAdapter() }
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentRecipesBinding.inflate(inflater,container,false)
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
 
-        binding.shimmerFrameLayout.showShimmer(true)
+        setupRecyclerView()
+        requestApiData()
+
         return binding.root
+    }
+
+    private fun requestApiData() {
+        mainViewModel.getRecipes(applyQueries())
+        mainViewModel.recipesResponse.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is NetworkResult.Success -> {
+                    hideShimmerEffect()
+                    response.data?.let { adapter.setData(it) }
+                }
+                is NetworkResult.Error -> {
+                    hideShimmerEffect()
+                    Toast.makeText(
+                        requireContext(),
+                        response.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                is NetworkResult.Loading -> {
+                    showShimmerEffect()
+                }
+            }
+        }
+    }
+
+    private fun applyQueries(): HashMap<String, String> {
+        val queries: HashMap<String, String> = HashMap()
+
+        queries["number"] = "50"
+        queries["apiKey"] = API_KEY
+        queries["type"] = "snack"
+        queries["diet"] = "vegan"
+        queries["addRecipeInformation"] = "true"
+        queries["fillIngredients"] = "true"
+        return queries
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        showShimmerEffect()
+    }
+
+    private fun showShimmerEffect() {
+        binding.shimmerFrameLayout.showShimmer(true)
+        binding.shimmerFrameLayout.visibility = View.VISIBLE
+        binding.recyclerView.visibility = View.GONE
+    }
+
+    private fun hideShimmerEffect() {
+        binding.shimmerFrameLayout.hideShimmer()
+        binding.shimmerFrameLayout.visibility = View.GONE
+        binding.recyclerView.visibility = View.VISIBLE
     }
 
     override fun onDestroy() {
